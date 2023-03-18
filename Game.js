dfec978445f81e1d2b835bd2023232b6ca2c2034
Lorenzo Hernandez <lorenzorhdez@hotmail.com>
2023-03-18 12:21:43 +0100

    primer intento

--- Game.js
+++ Game.js
@@ -1,19 +1,34 @@
 class game {
 
     constructor(questionArray) {
-        this.question = questionArray[0].question;
+        this.questions = questionArray;
+        this.question = questionArray[1].question;
         this.option = questionArray[0].options;
         this.answer = questionArray[0].answer;
-     };
+        this.initGame()
+        console.log(this.questions)
+     }
+     initGame() {
+        let question = this.questions[0].question;
+        let options = this.questions[0].options;
+        
+        let quest1 = document.querySelector(".preguntas")
+        quest1.innerHTML = question
+
+        let bottons = document.querySelector(".respuestas")
+        options.forEach(element => {
+            
+        });
+
+
+     }
     
      addNewQuestion () {
         this.question = document.querySelector(".preguntas");
      }
      addNewOptions() {
-        this.option = document.querySelector(".respuesta")
+        this.option = document.querySelector(".respuestas")
      }
-     
-    
 }
 let newGame = new game (questions);
 
