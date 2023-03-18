6c97f1bf42905a761cab77061b2d409edc96b798
Lorenzo Hernandez <lorenzorhdez@hotmail.com>
2023-03-18 11:08:34 +0100

    primer intento

---
+++ Game.js
@@ -0,0 +1,21 @@
+class game {
+
+    constructor(questionArray) {
+        this.question = questionArray[0].question;
+        this.option = questionArray[0].options;
+        this.answer = questionArray[0].answer;
+     };
+    
+     addNewQuestion () {
+        this.question = document.querySelector(".preguntas");
+     }
+     addNewOptions() {
+        this.option = document.querySelector(".respuesta")
+     }
+     
+    
+}
+let newGame = new game (questions);
+
+
+
--- js/Preguntas.js
+++ js/Preguntas.js
@@ -1,52 +1,52 @@
 let questions = [
     {
      question: "¿De que color es la bandera de España?",
-     obtions: ["Roja y verde", "Verde y azul", "Roja y amarilla", "Tricolor"],
+     options: ["Roja y verde", "Verde y azul", "Roja y amarilla", "Tricolor"],
      answer: 2
     },
     {
      question: "¿Cantos meses tiene un año?",
-     obtions: ["20", "12", "15", "10"],
+     options: ["20", "12", "15", "10"],
      answer: 1
     },
     {
      question: "¿Cual es el metal mas caro del mundo?",
-     obtions: ["Rodio", "Plata", "Oro", "Hierro"],
+     options: ["Rodio", "Plata", "Oro", "Hierro"],
      answer: 0
     },
     {
      question: "¿Cual es el simbolo qumico del hierro?",
-     obtions: ["Pb", "Sn", "Fe", "Co"],
+     options: ["Pb", "Sn", "Fe", "Co"],
      answer: 2
     },
     {
      question: "¿Cual es la capital de Rumanía?",
-     obtions: ["Sofia", "Budapest", "Belgrado", "Bucarest"],
+     options: ["Sofia", "Budapest", "Belgrado", "Bucarest"],
      answer: 3
     },
     {
      question: "¿Quien escribio la Odisea?",
-     obtions: ["Homero", "Vigilio", "Eugamon", "Socrates"],
+     options: ["Homero", "Vigilio", "Eugamon", "Socrates"],
      answer: 0  
     },
     {
      question: "¿Cual es el rio mas largo del mundo?",
-     obtions: ["Amazonas", "Nilo", "Misisipi", "Ganges"],
+     options: ["Amazonas", "Nilo", "Misisipi", "Ganges"],
      answer: 1
     },
     {
      question: "¿Donde se originaron los juegos olimpicos?",
-     obtions: ["Venezuela", "Italia", "Grecia", "España"],
+     options: ["Venezuela", "Italia", "Grecia", "España"],
      answer: 2
     },
     {
      question: "¿Cual es el lugar mas frio de la tierra?",
-     obtions: ["La Antartida", "El polo Norte", "El polo Sur", "Groelandia"],
+     options: ["La Antartida", "El polo Norte", "El polo Sur", "Groelandia"],
      answer: 0
     },
     {
      question: "¿Que año llego Cristobal Colon a America?",
-     obtions: ["1945", "1367", "1649", "1492"],
+     options: ["1945", "1367", "1649", "1492"],
      answer: 3
     },
 ]
