948956b4504f53c5314b56aa28b8cdb83f38330c
Lorenzo Hernandez <lorenzorhdez@hotmail.com>
2023-03-18 14:23:48 +0100

    js new

--- Game.js
+++ Game.js
@@ -2,34 +2,39 @@ class game {
 
     constructor(questionArray) {
         this.questions = questionArray;
-        this.question = questionArray[1].question;
-        this.option = questionArray[0].options;
-        this.answer = questionArray[0].answer;
         this.initGame()
-        console.log(this.questions)
      }
      initGame() {
-        let question = this.questions[0].question;
-        let options = this.questions[0].options;
+        let question = this.questions[2].question;
+        let options = this.questions[2].options;
+        let answer = this.questions[2].answer;
         
         let quest1 = document.querySelector(".preguntas")
         quest1.innerHTML = question
 
         let bottons = document.querySelector(".respuestas")
-        options.forEach(element => {
-            
-        });
+        options.forEach((element, index) => {
+            let crearBoton = document.createElement("button")
+            bottons.appendChild(crearBoton)
+            crearBoton.innerText = element
+            crearBoton.setAttribute("name", index)
+            crearBoton.addEventListener('click', function() {
+                let checkNumber = parseInt(this.getAttribute("name"));
+                
+                if (checkNumber === answer) {
+                    console.log("ok")
+                }
+                else {
+                  console.log("error")
+                }
 
+            });
+        });
 
      }
     
-     addNewQuestion () {
-        this.question = document.querySelector(".preguntas");
-     }
-     addNewOptions() {
-        this.option = document.querySelector(".respuestas")
-     }
 }
+
 let newGame = new game (questions);
 
 
