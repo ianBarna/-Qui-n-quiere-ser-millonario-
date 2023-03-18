2f4e985ac8f107ee38526c12075fadc621ab3d1d
Lorenzo Hernandez <lorenzorhdez@hotmail.com>
2023-03-18 09:50:40 +0100

    one commit

---
+++ js/scripts.js
@@ -0,0 +1,52 @@
+let questions = [
+    {
+     question: "¿De que color es la bandera de España?",
+     obtions: ["Roja y verde", "Verde y azul", "Roja y amarilla", "Tricolor"],
+     answer: 2
+    },
+    {
+     question: "¿Cantos meses tiene un año?",
+     obtions: ["20", "12", "15", "10"],
+     answer: 1
+    },
+    {
+     question: "¿Cual es el metal mas caro del mundo?",
+     obtions: ["Rodio", "Plata", "Oro", "Hierro"],
+     answer: 0
+    },
+    {
+     question: "¿Cual es el simbolo qumico del hierro?",
+     obtions: ["Pb", "Sn", "Fe", "Co"],
+     answer: 2
+    },
+    {
+     question: "¿Cual es la capital de Rumanía?",
+     obtions: ["Sofia", "Budapest", "Belgrado", "Bucarest"],
+     answer: 3
+    },
+    {
+     question: "¿Quien escribio la Odisea?",
+     obtions: ["Homero", "Vigilio", "Eugamon", "Socrates"],
+     answer: 0  
+    },
+    {
+     question: "¿Cual es el rio mas largo del mundo?",
+     obtions: ["Amazonas", "Nilo", "Misisipi", "Ganges"],
+     answer: 1
+    },
+    {
+     question: "¿Donde se originaron los juegos olimpicos?",
+     obtions: ["Venezuela", "Italia", "Grecia", "España"],
+     answer: 2
+    },
+    {
+     question: "¿Cual es el lugar mas frio de la tierra?",
+     obtions: ["La Antartida", "El polo Norte", "El polo Sur", "Groelandia"],
+     answer: 0
+    },
+    {
+     question: "¿Que año llego Cristobal Colon a America?",
+     obtions: ["1945", "1367", "1649", "1492"],
+     answer: 3
+    },
+]
